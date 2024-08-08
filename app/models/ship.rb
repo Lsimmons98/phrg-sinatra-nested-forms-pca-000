5ae539419542939cb817382baf5359dc55cd23ec
Liam Simmons <liam.simmons@powerhrg.com>
2024-08-08 11:01:29 -0400

    Add nested form for pirate and ships.

--- app/models/ship.rb
+++ app/models/ship.rb
@@ -1,2 +1,20 @@
 class Ship
+  attr_accessor :name, :type, :booty
+
+  @@ships = []
+
+  def initialize(ship)
+    @name = ship[:name]
+    @type = ship[:type]
+    @booty = ship[:booty]
+    @@ships << self
+  end
+
+  def self.clear
+    @@ships.clear
+  end
+
+  def self.all
+    @@ships
+  end
 end
